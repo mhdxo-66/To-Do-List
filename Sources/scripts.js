de324f047184bc431cf5aaf827e7ea5119ed374a
mhdxo-66 <mahdiarvndi99@gmail.com>
2026-06-27 23:07:03 +0330

    Added Edit Option, Used Pop Up For Add And Edit

--- Sources/scripts.js
+++ Sources/scripts.js
@@ -2,12 +2,34 @@
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function AddTsk() {
-  tasks.push(prompt("Write A Task"));
+  let task;
+  task = prompt("Write A Task");
+  if (task === "") {
+    alert(`Your Task Is Empty`);
+  } else if (task === null) {
+    alert(`Adding Task Cancelled`)
+  } else {
+    tasks.push(task);
+    alert(`The Task Of " ${tasks[tasks.length - 1]} " Added`);
+  }
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  alert(`The Task Of " ${tasks[tasks.length - 1]} " Added`);
 }
 function rm(i) {
   tasks.splice(i, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  tsklist();
+}
+function dt(i) {
+  let task;
+  task = prompt("Edit Your Task", tasks[i]);
+  if (task === "") {
+    alert(`You Write Empty Task`)
+  } else if (task === null) {
+    alert(`Editing Task Cancelled`)
+  } else {
+    tasks[i] = task;
+    alert(`Your Task Edited \n Final : ${tasks[i]}`)
+  }
   localStorage.setItem("tasks", JSON.stringify(tasks));
   tsklist();
 }
@@ -16,7 +38,7 @@ function tsklist() {
   let html = "";
   for (let i = 0; i < tasks.length; i++) {
     html += "<li>" + tasks[i] +
-      `<button id=rmbtn onclick='rm(${i})'>x</button></li>`
+      `<button id=rmbtn onclick='rm(${i})'>x</button><button id=dtbtn onclick='dt(${i})'>✏️</button></li>`
 
   }
   document.getElementById("list").innerHTML = html;
