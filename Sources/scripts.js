e7896ad3cb3289f7d0b3155e8ad53b3947dcb690
mhdxo-66 <mahdiarvndi99@gmail.com>
2026-06-27 21:51:17 +0330

    Merge Main Page And List Page

--- Sources/scripts.js
+++ Sources/scripts.js
@@ -1,11 +1,11 @@
 "use strict";
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-function addtsk() {
-  tasks.push(document.getElementById("tskinput").value);
+
+function AddTsk() {
+  tasks.push(prompt("Write A Task"));
   localStorage.setItem("tasks", JSON.stringify(tasks));
   alert(`The Task Of " ${tasks[tasks.length - 1]} " Added`);
 }
-
 function rm(i) {
   tasks.splice(i, 1);
   localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -16,7 +16,7 @@ function tsklist() {
   let html = "";
   for (let i = 0; i < tasks.length; i++) {
     html += "<li>" + tasks[i] +
-      `<button id=rmbtn onclick='rm(${i})'>x</button><button id="dtbtn">x</button></li>X`
+      `<button id=rmbtn onclick='rm(${i})'>x</button></li>`
 
   }
   document.getElementById("list").innerHTML = html;
